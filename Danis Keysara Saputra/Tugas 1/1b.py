0efd0d9f6f3c69af69f2f0ae382f71c5679808a1
Danis Keysara Saputra <128441399+Dakesha24@users.noreply.github.com>
2023-09-20 05:10:19 +0700

    1b.py
    No 1
    Buatlah sebuah fungsi penyelesaian yang tepat baik dengan menggunakan metode Bagi Dua ketika
    
    b. f(x) = e^x - x

--- Danis Keysara Saputra/Tugas 1/1b.py
+++ Danis Keysara Saputra/Tugas 1/1b.py
@@ -1,3 +1,30 @@
-No 1
-Buatlah sebuah fungsi penyelesaian yang tepat baik dengan menggunakan metode Bagi Dua ketika
-b. f(x) = e^x - x
+# Danis Keysara Saputra (2207275)
+# No 1b
+# Buatlah sebuah fungsi penyelesaian yang tepat baik dengan menggunakan metode Bagi Dua ketika
+# b. f(x) = e^x - x
+
+#solusi: tidak ditemukan, fungsi tidak memotong sb.x
+
+import numpy as np #library numpy untuk operasi numerik
+
+def my_bisection(f, a, b, e): #definisikan fungsi untuk mencari akar, beri parameternya
+    if np.sign(f(a)) == np.sign(f(b)): #meriksa apakah tanda nilai fungsi sama
+        
+        raise Exception('Tidak ada akar pada interval a dan b') #eksekusi jika tanda sama
+        
+    m = (a + b)/2 #eksekusi jika syarat terpenuhi, membagi interval jadi 2
+        
+    if np.abs(f(m)) < e: #jika terepenuhi, m akar hampiran
+        return m
+        
+    elif np.sign(f(a)) == np.sign(f(m)): #jika tanda nilai sama
+        return my_bisection(f,m,b,e) #interval baru = [m,b]
+        
+    elif np.sign(f(b)) == np.sign(f(m)): 
+        return my_bisection(f,a,m,e) #interval baru = [a,m]
+            
+            
+f = lambda x: np.exp(x) - x #definisikan fungsi soal menggunakan lambda
+c = my_bisection(f, -2, 2, 0.01) #memanggil fungsi, dengan argumen yang diberikan
+print("c = ", c)
+print("f(c) = ", f(c))
