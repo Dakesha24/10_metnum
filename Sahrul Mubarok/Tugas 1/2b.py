a54e6d1fca99e2c742c560d5585ef04961159bbf
Sahrul Mubarok <144931268+rullmbrk@users.noreply.github.com>
2023-09-20 21:19:51 +0700

    2b.py

--- Sahrul Mubarok/Tugas 1/2b.py
+++ Sahrul Mubarok/Tugas 1/2b.py
@@ -4,23 +4,23 @@
 
 import numpy as np
 
-def my_bisection(f, a, b, e, n): #definisikan fungsi untuk mencari akar, n untuk jumlah iterasi
-    if np.sign(f(a)) == np.sign(f(b)):  #meriksa apakah tanda nilai fungsi sam
-        raise Exception('Tidak ada akar pada interval a dan b')  #eksekusi jika tanda sama
+def my_bisection(f, a, b, e, n):  # Mendefinisikan fungsi pencarian akar dengan metode Bagi Dua, n untuk jumlah iterasi
+    if np.sign(f(a)) == np.sign(f(b)):  # Memeriksa apakah tanda nilai fungsi pada kedua ujung interval sama
+        raise Exception('Tidak ada akar pada interval a dan b')  # Jika tanda sama, lemparkan pengecualian
     
-    for i in range(n): #looping program hingga iterasi maksimal
-        m = (a + b) / 2 #eksekusi jika syarat terpenuhi, membagi interval jadi 2
+    for i in range(n):  # Looping program hingga iterasi maksimal
+        m = (a + b) / 2  # Membagi interval menjadi dua
         
-        if np.abs(f(m)) < e:  #jika terepenuhi, m akar hampiran
+        if np.abs(f(m)) < e:  # Jika nilai fungsi pada tengah interval kurang dari epsilon (toleransi), maka m adalah akar hampiran
             return m
             
         elif np.sign(f(a)) == np.sign(f(m)):
-            a = m #interval baru = [m, b]
+            a = m  # Interval baru = [m, b]
             
         elif np.sign(f(b)) == np.sign(f(m)):
-            b = m #interval baru = [a, m]
+            b = m  # Interval baru = [a, m]
 
-    #jika mencapai batas iterasi, kembalikan nilai fungsi pada iterasi terakhir
+    # Jika mencapai batas iterasi, kembalikan nilai fungsi pada iterasi terakhir
     last_value = (a + b) / 2
     return last_value
 
@@ -32,9 +32,10 @@ input_e = float(input("Masukkan toleransi galat (e): "))
 input_n = int(input("Masukkan jumlah iterasi (n): "))
 
 # Membuat fungsi dari input pengguna
-f = lambda x: eval(input_f) #mengevaluasi ekspresi atau kode Python yang diberikan sebagai string
+f = lambda x: eval(input_f)  # Mengevaluasi ekspresi atau kode Python yang diberikan sebagai string
 
 # Mencari akar dengan argumen yang diinput oleh pengguna
-c = my_bisection(f, input_a, input_b, input_e, input_n) #memanggil fungsi, dengan argumen yang diinput
-print("c =", c)
-print("f(c) =", f(c))
+c = my_bisection(f, input_a, input_b, input_e, input_n)  # Memanggil fungsi, dengan argumen yang diinput
+print("Akar c =", c)
+print("Nilai f(c) =", f(c))
+
