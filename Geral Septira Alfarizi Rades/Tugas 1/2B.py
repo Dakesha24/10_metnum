9a076cf5ffe5d7cc36099c704f1cb0241fc694ef
Grldprds <144988906+Grldprds@users.noreply.github.com>
2023-09-20 20:04:12 +0700

    2B.py

--- Geral Septira Alfarizi Rades/Tugas 1/2B.py
+++ Geral Septira Alfarizi Rades/Tugas 1/2B.py
@@ -1,40 +1,48 @@
-# Danis Keysara Saputra
 # No 2
 # 2. Buatlah modifikasi agar user dapat menginputkan fungsi maupun batas akar dan galatnya
 
-import numpy as np
+import numpy as np #library numpy untuk operasi numerik
 
-def my_bisection(f, a, b, e, n): #definisikan fungsi untuk mencari akar, n untuk jumlah iterasi
-    if np.sign(f(a)) == np.sign(f(b)):  #meriksa apakah tanda nilai fungsi sam
-        raise Exception('Tidak ada akar pada interval a dan b')  #eksekusi jika tanda sama
-    
-    for i in range(n): #looping program hingga iterasi maksimal
-        m = (a + b) / 2 #eksekusi jika syarat terpenuhi, membagi interval jadi 2
+def my_bisection(f, a, b, e, n): #definisikan fungsi untuk mencari akar dari sebuah fungsi f pada interval [a, b] dengan toleransi e dan jumlah iterasi `n`
+    if np.sign(f(a)) == np.sign(f(b)): #Periksa apakah ujung-ujung interval [a, b] memiliki tanda yang sama
+        raise Exception('Tidak ada akar pada interval a dan b') #akan memunculkan exception dengan pesan kesalahan
         
-        if np.abs(f(m)) < e:  #jika terepenuhi, m akar hampiran
-            return m
+    for i in range(n): # Perulangan untuk melakukan iterasi sebanyak n kali
+        m = (a + b)/2 #Hitung titik tengah interval [a, b]
+        
+           if np.abs(f(m)) < e: #Jika nilai absolut dari fungsi f(m) kurang dari toleransi e, maka m adalah akar fungsi
+        return m #Kembalikan akar fungsi m
             
-        elif np.sign(f(a)) == np.sign(f(m)):
-            a = m #interval baru = [m, b]
+        elif np.sign(f(a)) == np.sign(f(m)): #Jika kondisi tersebut terpenuhi, maka akar fungsi terletak di interval [m, b]
+            return my_bisection(f, m, b, e, n) #interval baru = [m,b]
             
-        elif np.sign(f(b)) == np.sign(f(m)):
-            b = m #interval baru = [a, m]
-
+        elif np.sign(f(b)) == np.sign(f(m)): #Jika kondisi tersebut terpenuhi, maka akar fungsi terletak di interval [a, m]
+            return my_bisection(f, a, m, e, n) #interval baru = [a, m]
+        
     #jika mencapai batas iterasi, kembalikan nilai fungsi pada iterasi terakhir
-    last_value = (a + b) / 2
+    last_value = (a + b) / 2 
     return last_value
 
+
 # Meminta user untuk memasukkan fungsi, interval, galat, dan iterasi
+# Input fungsi
 input_f = input("Masukkan fungsi f(x): ")
+# Input interval
 input_a = float(input("Masukkan interval bawah (a): "))
 input_b = float(input("Masukkan interval atas (b): "))
+# Input toleransi galat
 input_e = float(input("Masukkan toleransi galat (e): "))
+# Input jumlah iterasi
 input_n = int(input("Masukkan jumlah iterasi (n): "))
 
 # Membuat fungsi dari input pengguna
-f = lambda x: eval(input_f) #mengevaluasi ekspresi atau kode Python yang diberikan sebagai string
+# Menggunakan fungsi lambda untuk membuat fungsi sederhana
+f = lambda x: eval(input_f)
 
 # Mencari akar dengan argumen yang diinput oleh pengguna
-c = my_bisection(f, input_a, input_b, input_e, input_n) #memanggil fungsi, dengan argumen yang diinput
+c = my_bisection(f, input_a, input_b, input_e, input_n)
+
+# Output
 print("c =", c)
 print("f(c) =", f(c))
+
